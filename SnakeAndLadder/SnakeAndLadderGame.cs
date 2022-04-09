a22bdded9b6b554cf4ae4318db04a0e083316be9
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-10 01:33:59 +0530

    [Vidya] Add : Player Reaching The Winning Position Is Done

--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -18,18 +18,28 @@ namespace SnakeAndLadder
         }
         public void Game()
         {
-            Random random = new Random();
-            int option = random.Next(0, 3);
-            switch(option)
+            while (this.position < 100)
             {
-                case NO_PLAY:
-                    break;
-                case SNAKE: this.position -= this.DieRoll();
-                    break;
-                case LADDER: this.position += this.DieRoll();
-                    break;
+                Random random = new Random();
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        this.position += 0;
+                        break;
+                    case SNAKE:
+                        this.position -= this.DieRoll();
+                        if(this.position < 0)
+                        {
+                            this.position = 0;
+                        }
+                        break;
+                    case LADDER:
+                        this.position += this.DieRoll();
+                        break;
+                }
             }
-            Console.WriteLine("The option is " + this.position);
+            Console.WriteLine("The position is " + this.position);
         }
     }
 }
