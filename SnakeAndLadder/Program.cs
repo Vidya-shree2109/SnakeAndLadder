4155c388c0c91267ae106638475b570d4c244dfe
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-09 22:57:37 +0530

    [Vidya] Add : Single Player At Position 0 Implemented

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            SnakeAndLadderGame game = new SnakeAndLadderGame();
+        }
+    }
+}
