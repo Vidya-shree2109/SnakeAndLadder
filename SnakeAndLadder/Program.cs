2a6ee5e7fb14e22c823e1e76982b6646df026b80
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-10 18:14:30 +0530

    [Vidya] Refcatored : Two Player Game Is Implemented

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,14 +1,70 @@
-﻿using System;
-namespace SnakeAndLadder
-{
-    class Program
-    {
-        static void Main(String[] args)
-        {
-            SnakeAndLadderGame game = new SnakeAndLadderGame();
-            Console.WriteLine("The Die rolled number is " + game.DieRoll());
-            game.Game();
-        }
+﻿using SnakeAndLadder;
+Console.WriteLine("SNAKE AND LADDER GAME");
 
+SnakeAndLadderGame game = new SnakeAndLadderGame();
+game.start();
+public class Player
+{
+    public int position = 0;
+    public static Random random = new Random();
+    public int roll;
+    public int totalRoll;
+    /// <summary>
+    /// Moves the player
+    /// </summary>
+    public void Move(int movement)
+    {
+        position += movement;
+        if (position > 100)
+        {
+            position -= movement;
+            Console.WriteLine("You crossed 100 so moving back to last position");
+        }
+    }
+    /// <summary>
+    /// Rolls the dice
+    /// </summary>
+    public void RollDice()
+    {
+        roll = random.Next(1, 7);
+        totalRoll++;
+        Display();
+        Console.WriteLine("Player rolled: " + roll);
+        Move(roll);
+        Option();
+    }
+    /// <summary>
+    /// Displays player position
+    /// </summary>
+    public void Display()
+    {
+        Console.WriteLine("Player position: " + position);
+    }
+    /// <summary>
+    /// Options for snake and ladder
+    /// </summary>
+    public void Option()
+    {
+        int option = random.Next(0, 3);
+        switch (option)
+        {
+            case 0:
+                Console.WriteLine("No Play");
+                break;
+
+            case 1:
+                Console.WriteLine("You are on Snake");
+                Move(-roll);
+                break;
+
+            case 2:
+                Console.WriteLine("You found a ladder");
+                Move(roll);
+                break;
+
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
     }
 }
--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -8,45 +8,35 @@ namespace SnakeAndLadder
 {
     public class SnakeAndLadderGame
     {
-        int position = 0, count = 0;
-        const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
-        public int DieRoll()
+        Player P1 = new Player();
+        Player P2 = new Player();
+        Random random = new Random();
+        /// <summary>
+        /// Starts the game
+        /// </summary>
+        public void start()
         {
-            Random random = new Random();
-            int diePosition = random.Next(1, 7);
-            return diePosition;
-        }
-        public void Game()
-        {
-            while (this.position < 100)
+            while (P1.position < 100 && P2.position < 100)
             {
-                Random random = new Random();
-                int option = random.Next(0, 3);
-                switch (option)
-                {
-                    case NO_PLAY:
-                        this.position += 0;
-                        break;
-                    case SNAKE:
-                        this.position -= this.DieRoll();
-                        if(this.position < 0)
-                        {
-                            this.position = 0;
-                        }
-                        break;
-                    case LADDER:
-                        int roll = this.DieRoll();
-                        this.position += roll;
-                        if (this.position > 100)
-                        {
-                            this.position -= roll;
-                        }
-                        break;
-                }
-                Console.WriteLine("The position after every die roll is : " + this.position);
-                count++;
+                Console.WriteLine("Player1 : ");
+                P1.RollDice();
+                P1.Display();
+                if (P1.position == 100)
+                    break;
+                P2.RollDice();
+                P2.Display();
             }
-            Console.WriteLine("The number of times the dice played : " + count++);
+            Console.WriteLine("Player1 total rolled:" + P1.totalRoll);
+            Console.WriteLine("Player2 total rolled:" + P2.totalRoll);
+
+            if (P2.position == 100)
+            {
+                Console.WriteLine("Player 2 has won!!");
+            }
+            else
+                Console.WriteLine("Player 1 has won!!");
         }
+
     }
+
 }
